e3a6b11e0272ed745ed320202cbad903657d7072
aahmadi15 <46736794+aahmadi15@users.noreply.github.com>
2019-04-12 18:19:46 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,74 @@
+var HTTP_PORT = process.env.PORT || 3000;
+const express = require("express");
+const exphbs = require('express-handlebars');
+const path = require ("path");
+const app = express();
+var bodyParser = require('body-parser');
 var bitcore = require('bitcore-lib');
 var Insight = require('bitcore-explorers').Insight;
-var privateKeyWIF = 'cV8BaciFsJVA3B3Q9fjXmkeofjBfFiiA4Z9nTrSJCtonwUg2BKzr';
-
+var privateKeyWIF = 'cV8BaciFsJVA3B3Q9fjXmkeofjBfFiiA4Z9nTrSJCtonwUg2BKzr'; //Private key intended for address mgNgSfeNjxYsLtY5SDE8XbetwUuwqrLjJ1
 var privateKey = bitcore.PrivateKey.fromWIF(privateKeyWIF);
-var address = privateKey.toAddress();//sender address
-
-console.log('address');
-console.log(address);
-
-var privateKeyWIF2 = 'cMjSqy8zfuJYjJ5uQKpYbTzCjfupkwKcM4KJW54x3uMM2LGYqKnB';
-
-var privateKey2 = bitcore.PrivateKey.fromWIF(privateKeyWIF2);
-var address2 = privateKey2.toAddress(); //recieving address
 
 
-console.log('address2:');
-console.log(address2);
+app.engine(".hbs", exphbs({ extname: ".hbs"}));
+app.set("view engine", ".hbs");
 
-var insight = new Insight('testnet');
-//queries a function run by bitpays insight server allows querying of node
+app.use(express.static(path.join(__dirname, '/public')));
 
-var tx = bitcore.Transaction();
-insight.getUnspentUtxos(address, function(err, utxos){
-if (err){
 
-}
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-else {
-    console.log('Unspent Transaction Amount'+ utxos); //array of unspent outputs
-    tx.from(utxos);
-    tx.to(address2, 5000);
-    tx.change(address);
-    tx.fee(10000);
-    tx.sign(privateKey);//private key associated with address in input transaction
-    //console.log('transaction:');
-    tx.sign(privateKey2);
-    console.log(tx.toObject());
-    console.log('serialized output');
-    tx.serialize();
-    insight.broadcast(tx, function(err, returnedTxId){
-       if (err){
-            console.log(err);
-        }
-        //else
-        //{
-            console.log('Successful Broadcast: ' + returnedTxId);
-        //}
-    });
-    //https://live.blockcypher.com/btc-testnet/address/mgNgSfeNjxYsLtY5SDE8XbetwUuwqrLjJ1/ checks testnet bitcoins
-}
+app.post('/transaction', function(req,res){ //purpose to render the transactions page
+    res.render('Transaction', {});
+
 })
+
+app.post('/chart', function(req, res){
+    res.render('mychart', {}); //used to render the chart with on click command
+})
+
+app.post('/buy', function(req, res){
+    var insight = new Insight('testnet');
+    //queries a function run by bitpays insight server allows querying of node
+    var add1 = req.body.address;
+    console.log(add1);
+    var add2 = req.body.address2;
+    console.log(add2);
+    var dep = req.body.deposit;
+    insight.getUnspentUtxos(add1, function(err, utxos){
+    
+    var privateKeyWIF = 'cV8BaciFsJVA3B3Q9fjXmkeofjBfFiiA4Z9nTrSJCtonwUg2BKzr';//used for signing so transaction hex value can be generated specifically address mgNgSfeNjxYsLtY5SDE8XbetwUuwqrLjJ1
+
+    var privateKey = bitcore.PrivateKey.fromWIF(privateKeyWIF);
+
+        var tx = bitcore.Transaction();
+        console.log('Unspent Transaction Amount'+ utxos); //array of unspent outputs
+        tx.from(utxos);
+        tx.to(req.body.address2, req.body.deposit);
+        tx.change(req.body.address);
+        tx.sign(privateKey);
+        console.log(tx.toObject());
+        console.log(tx);
+        
+        res.redirect('https://live.blockcypher.com/btc-testnet/pushtx/'); //you must copy and paste the hex transaction value posted in console log when prompted to
+})
+});
+
+app.post('/redirect', function(request, response){ //used to check validity of an address
+        response.redirect('https://live.blockcypher.com/btc-testnet/address/'+request.body.address);
+})
+
+app.get("/", function(req, res) //gets the front page
+{     
+    res.render('Markdale',{})
+})
+app.post("/", function(req, res) //necessary to post all data for front page.
+{   
+    res.render('Markdale',
+    {})
+});
+
+var server = app.listen(HTTP_PORT, function () {
+	console.log('Listening on port ' + HTTP_PORT); //ensures the server is hosted on port 3000
+})
+
